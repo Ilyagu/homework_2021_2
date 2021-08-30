25bd842813bdff29fc2ac50ac281ecd10dd3705b
Ilyagu <inagdimaev@mail.ru>
2021-08-30 15:45:04 +0300

    formated

--- source/minmax.js
+++ source/minmax.js
@@ -5,10 +5,11 @@
  * результат ввиде массива из минимального и максимального значения.
 */
 const minmax = arr => {
-  if ((typeof arr) !='string') return [undefined, undefined];
-  let newArr = arr.replace(/[\\,]/g, '');
-  newArr = newArr.split(' ').filter(item=>item !== '')
-  newArr = newArr.map(item=>Number(item))
-  newArr = newArr.filter(item=>!isNaN(item));
-  return (newArr.length === 0) ? [undefined, undefined] : [Math.min(...newArr), Math.max(...newArr)]
+  if (typeof arr !== 'string') {
+    return [undefined, undefined];
+  }
+  let sortedItems = arr.replace(/[\\,]/g, '');
+  sortedItems = sortedItems.split(' ').filter(item => item !== '')
+  const result = sortedItems.map(item=>Number(item)).filter(item=>!isNaN(item));
+  return (result.length === 0) ? [undefined, undefined] : [Math.min(...result), Math.max(...result)]
 };
