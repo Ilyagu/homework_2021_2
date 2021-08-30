a27e0ef321bc5341e2ff6e80073d1ab12d2d8076
Ilyagu <inagdimaev@mail.ru>
2021-08-30 18:54:56 +0300

    formated code

--- source/minmax.js
+++ source/minmax.js
@@ -1,15 +1,21 @@
 'use strict';
 
-/** 
- * Функция minmax нахожит минимальное и максимальное значение в строке 
- * результат ввиде массива из минимального и максимального значения.
-*/
+/**
+ * Это описание функции minmax
+ * @param {string} st - Строка с числами
+ * @returns {array} Массив из минимального
+ * и максимального элементов в строке
+ */
 const minmax = st => {
+
   if (typeof st !== 'string') {
     return [undefined, undefined];
   }
-  let sortedItems = st.replace(/[\\,]/g, '');
-  sortedItems = sortedItems.split(' ').filter(item => item !== '')
-  const result = sortedItems.map(item=>Number(item)).filter(item=>!isNaN(item));
-  return (result.length === 0) ? [undefined, undefined] : [Math.min(...result), Math.max(...result)]
-};
+  
+  const formatedString = st.replace(/[\\,]/g, '');
+  const itemsArray = formatedString.split(' ').filter(item => item !== '');
+  const result = itemsArray.map(item => Number(item)).filter(item => !isNaN(item));
+
+  return (result.length === 0) ? [undefined, undefined] :
+    [Math.min(...result), Math.max(...result)];
+}
