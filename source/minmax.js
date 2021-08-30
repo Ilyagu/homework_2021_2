cd181c3418605702da296d2d635d3161ed1f0d73
Ilyagu <inagdimaev@mail.ru>
2021-08-30 15:46:05 +0300

    formated

--- source/minmax.js
+++ source/minmax.js
@@ -4,11 +4,11 @@
  * Функция minmax нахожит минимальное и максимальное значение в строке 
  * результат ввиде массива из минимального и максимального значения.
 */
-const minmax = arr => {
-  if (typeof arr !== 'string') {
+const minmax = st => {
+  if (typeof st !== 'string') {
     return [undefined, undefined];
   }
-  let sortedItems = arr.replace(/[\\,]/g, '');
+  let sortedItems = st.replace(/[\\,]/g, '');
   sortedItems = sortedItems.split(' ').filter(item => item !== '')
   const result = sortedItems.map(item=>Number(item)).filter(item=>!isNaN(item));
   return (result.length === 0) ? [undefined, undefined] : [Math.min(...result), Math.max(...result)]
